8bfac58e8f61dd8459ceee934af47e070a69fccb
Ayman Moukhtar <ayman.moukhtar@outlook.com>
2019-07-06 15:08:13 +0200

    added enemy AI

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+}
